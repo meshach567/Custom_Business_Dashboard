b459ffe41bc721651847f380872d0429668a95f2
Meshach Arinze <arinzemeshach123@gmail.com>
2025-06-13 17:12:07 +0100

    chore: remove the configModule and ConfigService fri  imports and inject

--- business_backend/src/config/typeorm.config.ts
+++ business_backend/src/config/typeorm.config.ts
@@ -1,21 +1,23 @@
-// src/config/typeorm.config.ts
-import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { User } from '../users/entities/user.entity';
-import { Sale } from '../sales/entities/sale.entity';
-import { Product } from '../inventory/entities/product.entity';
+// // src/config/typeorm.config.ts
 
-export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
-  imports: [ConfigModule],
-  inject: [ConfigService],
-  useFactory: async (configService: ConfigService) => ({
-    type: 'postgres',
-    host: configService.get<string>('DB_HOST'),
-    port: configService.get<number>('DB_PORT'),
-    username: configService.get<string>('DB_USERNAME'),
-    password: configService.get<string>('DB_PASSWORD'),
-    database: configService.get<string>('DB_NAME'),
-    entities: [User, Sale, Product],
-    synchronize: true, // false in production!
-  }),
-};
+// import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { User } from '../users/entities/user.entity';
+// import { Sale } from '../sales/entities/sale.entity';
+// import { Product } from '../inventory/entities/product.entity';
+
+// export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+//   imports: [ConfigModule],
+//   inject: [ConfigService],
+//   useFactory: async (configService: ConfigService) => ({
+//     type: 'postgres',
+//     host: configService.get<string>('DB_HOST'),
+//     port: parseInt(configService.get<string>('DB_PORT', '5432')),
+//     username: configService.get<string>('DB_USERNAME'),
+//     password: configService.get<string>('DB_PASSWORD'),
+//     database: configService.get<string>('DB_NAME'),
+//     entities: [__dirname, '../**/*.entity{.ts,.js}', User, Sale, Product],
+//     synchronize: configService.get<string>('NODE_ENV') !== 'production',
+//     autoLoadEntities: true,
+//   }),
+// };
